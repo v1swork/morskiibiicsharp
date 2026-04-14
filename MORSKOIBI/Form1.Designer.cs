79f761e116ae2d03485a658ef24beac61a57c234
Мокроусов Равиль <mokrousov_r@StOskol.loc>
2026-04-14 11:11:07 +0300

    first commit

---
+++ MORSKOIBI/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MORSKOIBI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelPlayer = new System.Windows.Forms.Panel();
+            this.PanelEnemy = new System.Windows.Forms.Panel();
+            this.labelstatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PanelPlayer
+            // 
+            this.PanelPlayer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelPlayer.Location = new System.Drawing.Point(40, 59);
+            this.PanelPlayer.Name = "PanelPlayer";
+            this.PanelPlayer.Size = new System.Drawing.Size(320, 320);
+            this.PanelPlayer.TabIndex = 0;
+            // 
+            // PanelEnemy
+            // 
+            this.PanelEnemy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PanelEnemy.Location = new System.Drawing.Point(442, 59);
+            this.PanelEnemy.Name = "PanelEnemy";
+            this.PanelEnemy.Size = new System.Drawing.Size(320, 320);
+            this.PanelEnemy.TabIndex = 1;
+            // 
+            // labelstatus
+            // 
+            this.labelstatus.AutoSize = true;
+            this.labelstatus.Location = new System.Drawing.Point(364, 27);
+            this.labelstatus.Name = "labelstatus";
+            this.labelstatus.Size = new System.Drawing.Size(80, 13);
+            this.labelstatus.TabIndex = 2;
+            this.labelstatus.Text = "ХОД: ИГРОКА";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelstatus);
+            this.Controls.Add(this.PanelEnemy);
+            this.Controls.Add(this.PanelPlayer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Морской Бiй";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelPlayer;
+        private System.Windows.Forms.Panel PanelEnemy;
+        private System.Windows.Forms.Label labelstatus;
+    }
+}
+
---
+++ MORSKOIBI/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MORSKOIBI
+{
+    public partial class Form1 : Form
+    {
+        private Button[,] playerButtons = new Button[10, 10];
+        private Button[,] enemyButtons = new Button[10, 10];
+
+        private int[,] playerBoard = new int[10, 10];
+        private int[,] enemyBoard = new int[10, 10];
+        // 0 — пусто
+        // 1 - корабль
+        // 2 - попадание
+        // 3 - мимо
+         
+        public Form1()
+        {
+            InitializeComponent();
+            CreateBoards();
+            PlaceShip();
+            DrawPlayerShips();
+        }
+
+        private void CreateBoards()
+        {
+            int cellSize = 29;
+            int leftMargin = 23;
+            int topMargin = 23;
+
+            string[] letters = { "А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К" };
+
+            for (int i = 0; i < 10; i++)
+            {
+                Label playerLetter = new Label();
+                playerLetter.Text = letters[i];
+                playerLetter.AutoSize = false;
+                playerLetter.Size = new Size(cellSize, topMargin);
+                playerLetter.TextAlign = ContentAlignment.MiddleCenter;
+                playerLetter.Location = new Point(leftMargin + i * cellSize,0);
+                PanelPlayer.Controls.Add(playerLetter);
+
+                Label enemyLetter = new Label();
+                enemyLetter.Text = letters[i];
+                enemyLetter.AutoSize = false;
+                enemyLetter.Size = new Size(cellSize, topMargin);
+                enemyLetter.TextAlign = ContentAlignment.MiddleCenter;
+                enemyLetter.Location = new Point(leftMargin + i * cellSize, 0);
+                PanelEnemy.Controls.Add(enemyLetter);
+
+                Label playerCiferki = new Label();
+                playerCiferki.Text = (i + 1).ToString();
+                playerCiferki.AutoSize = false;
+                playerCiferki.Size = new Size(leftMargin,cellSize);
+                playerCiferki.TextAlign = ContentAlignment.MiddleCenter;
+                playerCiferki.Location = new Point(0,topMargin + i * cellSize);
+                PanelPlayer.Controls.Add(playerCiferki);
+
+                Label enemyCiferki = new Label();
+                enemyCiferki.Text = (i + 1).ToString();
+                enemyCiferki.AutoSize = false;
+                enemyCiferki.Size = new Size(leftMargin, cellSize);
+                enemyCiferki.TextAlign = ContentAlignment.MiddleCenter;
+                enemyCiferki.Location = new Point(0, topMargin + i * cellSize);
+                PanelEnemy  .Controls.Add(enemyCiferki);
+
+
+
+            }
+            for (int row = 0; row <10;row++)
+            {
+                for(int col =0; col < 10;col++)
+                {
+                    Button playerButton = new Button();
+                    playerButton.Size = new Size(cellSize, cellSize);
+                    playerButton.Location = new Point(leftMargin + col * cellSize, topMargin + row * cellSize);
+                    playerButton.Enabled = false;
+
+                    PanelPlayer.Controls.Add(playerButton);
+                    playerButtons[row, col] = playerButton;
+
+                    Button enemyButton = new Button();
+                    enemyButton.Size = new Size(cellSize, cellSize);
+                    enemyButton.Location = new Point(leftMargin + col * cellSize, topMargin + row * cellSize);
+                    enemyButton.Enabled = true;
+
+                    enemyButton.Tag = new Point(row, col);
+                    enemyButton.Click += enemyButton_Click;
+
+                    PanelEnemy.Controls.Add(enemyButton);
+                    enemyButtons[row, col] = enemyButton;
+
+                } 
+            }
+        }
+
+        private void PlaceShip()
+        {
+            //Random random = new Random();
+            //int shipToPlace = 10;
+            //int placed = 0;
+
+            //while (placed < shipToPlace)
+            //{
+            //    int row = random.Next(0, 10);
+            //    int col = random.Next(0, 10);
+
+            //    if (enemyBoard[row, col] == 0)
+            //    {
+            //        enemyBoard[row, col] = 1;
+            //        placed++;
+            //    }
+            //}
+            //placed = 0;
+            //while (placed < shipToPlace)
+            //{
+            //    int row = random.Next(0, 10);
+            //    int col = random.Next(0, 10);
+            //    if (playerBoard[row, col] == 0)
+            //    {
+            //        playerBoard[row, col] = 1;
+            //        placed++;
+            //    }
+
+            //}
+            PlaceShipForBoard(enemyBoard);
+            PlaceShipForBoard(playerBoard);
+        }
+        private void PlaceShipForBoard(int[,] board)
+        {
+            Random random = new Random();
+            int[] ships = { 4, 3, 3, 2, 2, 2, 1, 1, 1, 1 };
+
+            foreach (int shipSize in ships)
+            { 
+                bool placed = false;
+                int row = random.Next(0, 10);
+                int col = random.Next(0, 10);
+                while (!placed)
+                { 
+                    
+
+                    bool horizontal = random.Next(2) == 0;
+                    //проверка
+                    if (CanPlaceShip(board, row, col, shipSize, horizontal))
+                    {
+                        for (int i = 0; i < shipSize; i++)
+                        {
+                            if (horizontal)
+                                board[row, col + i] = 1;
+                            else
+                                board[row + i, col] = 1;
+                        }
+                        placed = true;
+                        
+                    }
+                }
+            }
+        }
+
+        private bool CanPlaceShip(int[,] board, int row, int col, int size, bool horizontal)
+        {
+            if (horizontal)
+            {
+                if (col + size > 10)
+                    return false;
+
+                for (int i = 0; i < size; i++)
+                {
+                    if (board[row, col + i] != 0)
+                        return false;
+                }
+            }
+            else 
+            {
+                if (row + size > 10)
+                    return false;
+
+                for (int i = 0; i < size; i++) 
+                {
+                    if (board[row + i, col] != 0)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        
+
+        // 1 - 4
+        // 2 - 3
+        // 3 - 2
+        // 4 - 1
+
+        private void DrawPlayerShips()
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    if (playerBoard[row, col] == 1)
+                    {
+                        playerButtons[row, col].BackColor = Color.Gray;
+                    }
+                }
+            }
+        }
+
+        private void enemyButton_Click(object sender, EventArgs e)
+        { 
+            Button clickedButton = (Button)sender;
+            Point point = (Point)clickedButton.Tag;
+
+            int row = point.X; 
+            int col = point.Y;
+
+            if (enemyBoard[row, col] == 2 || enemyBoard[row, col] == 3)
+                return;
+
+            if (enemyBoard[row, col] == 1)
+            {
+                enemyBoard[row, col] = 2;
+                clickedButton.BackColor = Color.Red;
+                clickedButton.Text = "X";
+                labelstatus.Text = "Попадание!!!";
+
+                if (CheckWin(enemyBoard))
+                {
+                    labelstatus.Text = "Вы победили!";
+                    MessageBox.Show("Ты победил");
+                }
+            }
+            else 
+            { 
+                playerBoard [row, col] = 3;
+                clickedButton.BackColor = Color.White;
+                clickedButton.Text = "*";
+                labelstatus.Text = "Промах!!!";
+            }
+            if (!CheckWin(enemyBoard))
+            {
+                EnemyTurn();
+            }
+            
+        }
+
+        private bool CheckWin(int[,] board)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    if (board[row, col] == 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void EnemyTurn()
+        { 
+            Random random = new Random();
+
+            int row, col;
+
+            while (true)
+            {
+                row = random.Next(0, 10);
+                col = random.Next(0, 10);
+
+                if (playerBoard[row, col] == 2 || 
+                    playerBoard[row, col] == 3)
+                    continue;
+
+                break;
+            }
+
+            if (playerBoard[row, col] == 1)
+            {
+                playerBoard[row, col] = 2;
+                playerButtons[row, col].BackColor = Color.Red;
+                playerButtons[row, col].Text = "X";
+                labelstatus.Text = "Бот попал!";
+
+                if (CheckWin(playerBoard))
+                {
+                    labelstatus.Text = "Вы проирали!";
+                    MessageBox.Show("Ты проиграл");
+                }
+            }
+            else
+            {
+                playerBoard[row, col] = 3;
+                playerButtons[row, col].BackColor = Color.White;
+                playerButtons[row, col].Text = "*";
+                labelstatus.Text = "Бот промазал";
+            }
+        }
+    }
+}
